4cb78889fc01f3ca9186c553764127a4379b4463
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-09-20 21:04:22 +0200

    Add animal schema to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
-/* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg NUMERIC(4, 2) NOT NULL
 );
