86b7c94caa3580c8104e98c8c20627992c6b8534
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-09-21 23:35:53 +0200

    Add new column called species to snimals table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 
 CREATE TABLE animals (
   id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
+  name VARCHAR(50) NOT NULL,
   date_of_birth DATE NOT NULL,
   escape_attempts INT NOT NULL,
   neutered BOOLEAN NOT NULL,
-  weight_kg NUMERIC(4, 2) NOT NULL
+  weight_kg NUMERIC(5, 2) NOT NULL,
+  species VARCHAR(50)
 );
