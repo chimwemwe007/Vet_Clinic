782ccb17dd73819c4983c048e871011234fe296b
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-09-21 22:38:19 +0200

    Add query for animals

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+
+SELECT * FROM animals WHERE date_of_birth BETWEEN date '2016-01-01' AND date '2019-01-01';
+
+SELECT * FROM animals WHERE neutered=true AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name='Pikachu' OR name='Agumon';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered=true;
+
+SELECT * FROM animals WHERE NOT name='Gabumon';
+
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
